e434f7d4b64c3c98d3a82e1ffdab7e358d2664ac
ocenzurowano <skarpetownik@gmail.com>
2014-05-27 19:45:31 +0200

    wszystko

--- Psychotest/Atrybut.cs
+++ Psychotest/Atrybut.cs
@@ -9,11 +9,18 @@ namespace Psychotest
     class Atrybut
     {
         string nazwa;
-        List<string> wartosci;
+        List<string> wartosci = new List<string>();
+
         public Atrybut(string n, List<string> w)
         {
             this.nazwa = n;
-            
+            this.wartosci = w;
+        }
+        public Atrybut(string n, string[] w)
+        {
+            this.nazwa = n;
+            for (int i = 0; i < w.Length; i++)
+                this.wartosci.Add(w[i]);
         }
     }
 }
