a25802b50669d4592b90815965f26195a55133c6
ocenzurowano <skarpetownik@gmail.com>
2014-05-27 23:04:48 +0200

    klasy dane i attr

---
+++ Psychotest/Atrybut.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Psychotest
+{
+    class Atrybut
+    {
+        public string nazwa;
+        List<string> mozliwosci;
+    }
+}
---
+++ Psychotest/Dane.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Psychotest
+{
+    class Dane
+    {
+        List<string> odpowiedzi;//możliwe odpowiedzi np.: "pedał" "nie pedał"
+        public List<Atrybut> atrybuty;//atrybuty i ich możliwe wartości
+        public int kol, wier;//ilość kolumn i wierszy danych treningowych
+        public int[,] dane_treningowe;//dane do treningu z dodatkową kolumną na odpowiedź
+    }
+}
