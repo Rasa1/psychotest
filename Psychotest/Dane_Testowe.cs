4bd82becc7519f727271fb2ba600c6f935e4c0e5
ocenzurowano <skarpetownik@gmail.com>
2014-05-27 19:19:33 +0200

    dane testowe i atrybuty

---
+++ Psychotest/Dane_Testowe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Psychotest
+{
+    class Dane_Testowe
+    {
+        List<Atrybut> Lista_Atrybutow;
+
+    }
+}
