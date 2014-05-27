6b3b4a7f8d1b562e92b8828b0d6e973f6978bbcf
ocenzurowano <skarpetownik@gmail.com>
2014-05-27 23:21:21 +0200

    kolejne metody

--- Psychotest/Atrybut.cs
+++ Psychotest/Atrybut.cs
@@ -8,7 +8,35 @@ namespace Psychotest
 {
     class Atrybut
     {
-        public string nazwa;
-        List<string> mozliwosci;
+        public string nazwa;//nazwa atrybutu np.:outlook albo windy
+        List<string> mozliwosci;//lista możliwości np.:[sunny, overcast, rain]
+
+        public void AddMozliwosc(string m)//tej metody możesz użyć żeby dodawać kolejne możliwości jedna po drugiej 
+        {
+            this.mozliwosci.Add(m);
+        }
+        public void Coppy(Atrybut kopiuj_z)
+        {
+            this.nazwa = kopiuj_z.nazwa;
+            for (int i = 0; i < kopiuj_z.mozliwosci.Count; i++)
+                this.mozliwosci[i] = kopiuj_z.mozliwosci[i];
+        }
+
+        Atrybut(string n)
+        {
+            this.nazwa = n;
+        }
+        Atrybut(string n,string[] m)
+        {
+            this.nazwa = n;
+            for (int i = 0; i < m.Length; i++)
+                this.mozliwosci.Add(m[i]);
+        }
+        Atrybut(string n, List<string> m)
+        {
+            this.nazwa = n;
+            for (int i = 0; i < m.Count; i++)
+                this.mozliwosci.Add(m[i]);
+        }
     }
 }
--- Psychotest/Dane.cs
+++ Psychotest/Dane.cs
@@ -12,5 +12,20 @@ namespace Psychotest
         public List<Atrybut> atrybuty;//atrybuty i ich możliwe wartości
         public int kol, wier;//ilość kolumn i wierszy danych treningowych
         public int[,] dane_treningowe;//dane do treningu z dodatkową kolumną na odpowiedź
+
+        Dane(int k, int w)
+        {
+            this.kol = k;
+            this.wier = w;
+            this.dane_treningowe = new int[wier, kol + 1];
+        }
+        Dane(int k, int w,List<Atrybut> attr,List<string> odp)
+        {
+            this.kol = k;
+            this.wier = w;
+            this.dane_treningowe = new int[wier, kol + 1];
+            for(int i=0;i<attr.Count();i++)
+                
+        }
     }
 }
